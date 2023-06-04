ad12428b77d7a3b5dddf7fceec91fae4dc362f19
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2023-06-03 22:13:09 -0700

    Added some logic to catch invalid user inputs, clear user_guesses for each new game, and prevent user from guessing same number multiple times.

--- src/main.py
+++ src/main.py
@@ -7,41 +7,61 @@ print(logo)
 
 correct_num = random.randint(1, 100)
 guesses_left = 0
+user_guess = 0
 play_game = True
+user_guesses = []
 
 def start_game():
     global guesses_left
+    user_guesses = []
     while guesses_left >= 0:
-        user_guess = int(input("Guess a number! "))
-        if user_guess != correct_num and guesses_left == 1:
-            print(f"Game over! The number was {correct_num}")
-            break
-        elif user_guess > correct_num:
-            print("Lower")
-            guesses_left -= 1
-        elif user_guess < correct_num:
-            print("Higher!")
-            guesses_left -= 1
-        elif user_guess == correct_num:
-            print("You got it, you win!")
-            break
+      #  global user_guesses
+        try:
+            user_guess = int(input(f"You have guessed {user_guesses} so far. Guess a number! \nYou have {guesses_left} guesses left  "))
+            if user_guess in range(1, 100):
+                if user_guess in user_guesses:
+                    print("You guessed that already!")
+                else:
+                    user_guesses.append(user_guess)
+                    if user_guess != correct_num and guesses_left == 1:
+                        print(f"Game over! The number was {correct_num}")
+                        break
+                    elif user_guess > correct_num:
+                        print("Lower")
+                        guesses_left -= 1
+                    elif user_guess < correct_num:
+                        print("Higher!")
+                        guesses_left -= 1
+                    elif user_guess == correct_num:
+                        print("You got it, you win!")
+                        break
+        except: 
+            print(f"Try again, that was not a number.")
         
 
 
 
 
 while play_game == True:
-    user_play = input("Would you like to play a game? ").lower()
-    if user_play == "no":
+    user_play = input("Would you like to play a game? 'y' or 'n' ").lower()
+    if user_play == 'n':
         play_game = False
         print("Goodbye")
-    elif user_play == 'yes':
-        difficulty = input("What difficulty would you like to play on? 'Easy' or 'Hard'? ").lower()
+    elif user_play == 'y':
+        play_game = True
+        difficulty = input("What difficulty would you like to play on? 'Easy: 10 lives' or 'Hard: 5 lives'? ").lower()
         if difficulty == "easy":
             guesses_left += 10
         elif difficulty == 'hard':
             guesses_left += 5
-    start_game()
+    else:
+        play_game = False
+        print("Invalid Input: Try again")
+
+    if play_game == True:
+        start_game()
+
+        
         
 
 
